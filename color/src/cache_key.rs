3380034c7c1fb018b7d7c738e4d0dd22224f4c39
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-12-12 14:16:45 +0000

    Some more base cases
    Add the "active" implementations
    
    Add bytemuck impl
    
    Address more review feedback

--- color/src/cache_key.rs
+++ color/src/cache_key.rs
@@ -23,11 +23,26 @@
 use core::hash::{Hash, Hasher};
 
 /// A key usable in a hashmap to compare the bit representation
-/// types containing colours.
+/// types containing colors.
 ///
 /// See the [module level docs](self) for more information.
 #[derive(Debug, Copy, Clone)]
+#[repr(transparent)]
 pub struct CacheKey<T>(pub T);
+
+impl<T> CacheKey<T> {
+    /// Create a new `CacheKey`.
+    ///
+    /// All fields are public, so the struct constructor can also be used.
+    pub fn new(value: T) -> Self {
+        Self(value)
+    }
+
+    /// Get the inner value.
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
 
 // This module exists for these implementations:
 
@@ -35,7 +50,7 @@ pub struct CacheKey<T>(pub T);
 impl<T: BitEq> Eq for CacheKey<T> {}
 impl<T: BitEq> PartialEq for CacheKey<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.bit_eq(other)
+        self.0.bit_eq(&other.0)
     }
 }
 // If we implement Eq, BitEq's implementation matches that of the hash.
@@ -45,7 +60,7 @@ impl<T: BitHash> Hash for CacheKey<T> {
     }
 }
 
-/// An hash implementation for types normally wouldn't have one,
+/// A hash implementation for types which normally wouldn't have one,
 /// implemented using a hash of the bitwise equivalent types when needed.
 ///
 /// If a type is `BitHash` and `BitEq`, then it is important that the following property holds:
@@ -64,35 +79,132 @@ pub trait BitHash {
     // Intentionally no hash_slice for simplicity.
 }
 
-/// An equivalence relation for types normally wouldn't have
-/// one, implemented using a bitwise comparison when needed.
+impl BitHash for f32 {
+    fn bit_hash<H: Hasher>(&self, state: &mut H) {
+        self.to_bits().hash(state);
+    }
+}
+impl<T: BitHash, const N: usize> BitHash for [T; N] {
+    fn bit_hash<H: Hasher>(&self, state: &mut H) {
+        self[..].bit_hash(state);
+    }
+}
+
+impl<T: BitHash> BitHash for [T] {
+    fn bit_hash<H: Hasher>(&self, state: &mut H) {
+        // In theory, we should use `write_length_prefix`, which is unstable:
+        // https://github.com/rust-lang/rust/issues/96762
+        // We could do that by (unsafely) casting to `[CacheKey<T>]`, then
+        // using `Hash::hash` on the resulting slice.
+        state.write_usize(self.len());
+        for piece in self {
+            piece.bit_hash(state);
+        }
+    }
+}
+
+impl<T: BitHash> BitHash for &T {
+    fn bit_hash<H: Hasher>(&self, state: &mut H) {
+        T::bit_hash(*self, state);
+    }
+}
+
+// Don't BitHash tuples, not that important
+
+/// An equivalence relation for types which normally wouldn't have
+/// one, implemented using a bitwise comparison for floating point
+/// values.
 ///
 /// See the docs on [`Eq`] for more information.
 ///
 /// Useful for creating caches based on exact values.
 /// See the [module level docs](self) for more information.
 pub trait BitEq {
-    /// Returns true if `self` is the same "value" as other.
+    /// Returns true if `self` is equal to `other`.
+    ///
+    /// This need not use the semantically natural comparison operation
+    /// for the type; indeed floating point types should implement this
+    /// by comparing bit values.
     fn bit_eq(&self, other: &Self) -> bool;
     // Intentionally no bit_ne as would be added complexity for little gain
 }
 
-/// We already have an existing equivalence hash for these types, so just use that.
-impl<T> BitHash for T
-where
-    T: Hash,
-{
-    fn bit_hash<H: Hasher>(&self, state: &mut H) {
-        self.hash(state);
+impl BitEq for f32 {
+    fn bit_eq(&self, other: &Self) -> bool {
+        self.to_bits() == other.to_bits()
     }
 }
 
-/// We already have an existing equivalence relation for these types, so just use that.
-impl<T> BitEq for T
-where
-    T: PartialEq + Eq,
-{
+impl<T: BitEq, const N: usize> BitEq for [T; N] {
     fn bit_eq(&self, other: &Self) -> bool {
-        self.eq(other)
+        for i in 0..N {
+            if !self[i].bit_eq(&other[i]) {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl<T: BitEq> BitEq for [T] {
+    fn bit_eq(&self, other: &Self) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+        for (a, b) in self.iter().zip(other) {
+            if !a.bit_eq(b) {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl<T: BitEq> BitEq for &T {
+    fn bit_eq(&self, other: &Self) -> bool {
+        T::bit_eq(*self, *other)
+    }
+}
+
+// Don't BitEq tuples, not that important
+
+// Ideally we'd also have these implementations, but they cause conflicts
+// (in case std ever went mad and implemented Eq for f32, for example).
+// impl<T: Hash> BitHash for T {...}
+// impl<T: PartialEq + Eq> BitEq for T {...}
+
+#[cfg(test)]
+mod tests {
+    use super::CacheKey;
+    use crate::{parse_color, DynamicColor};
+
+    use std::collections::HashMap;
+
+    #[test]
+    fn bit_eq_hashmap() {
+        let mut map: HashMap<CacheKey<f32>, i32> = HashMap::new();
+        // The implementation for f32 is the base case.
+        assert!(map.insert(CacheKey(0.0), 0).is_none());
+        assert!(map.insert(CacheKey(-0.0), -1).is_none());
+        assert!(map.insert(CacheKey(1.0), 1).is_none());
+        assert!(map.insert(CacheKey(0.5), 5).is_none());
+
+        assert_eq!(map.get(&CacheKey(1.0)).unwrap(), &1);
+        assert_eq!(map.get(&CacheKey(0.0)).unwrap(), &0);
+        assert_eq!(map.remove(&CacheKey(-0.0)).unwrap(), -1);
+        assert!(!map.contains_key(&CacheKey(-0.0)));
+        assert_eq!(map.get(&CacheKey(0.5)).unwrap(), &5);
+    }
+    #[test]
+    fn bit_eq_color_hashmap() {
+        let mut map: HashMap<CacheKey<DynamicColor>, i32> = HashMap::new();
+
+        let red = parse_color("red").unwrap();
+        let red2 = parse_color("red").unwrap();
+        let other = parse_color("oklab(0.4 0.2 0.6)").unwrap();
+        assert!(map.insert(CacheKey(red), 10).is_none());
+        assert_eq!(map.insert(CacheKey(red2), 5).unwrap(), 10);
+        assert!(map.insert(CacheKey(other), 15).is_none());
+        assert_eq!(map.get(&CacheKey(other)).unwrap(), &15);
     }
 }
