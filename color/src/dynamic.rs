7eb305e904398a1eaa64fea9ea15f464e0c888a1
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-04-21 14:56:31 +0200

    Make comment a doc comment

--- color/src/dynamic.rs
+++ color/src/dynamic.rs
@@ -447,9 +447,9 @@ impl BitHash for DynamicColor {
     }
 }
 
-// Note that the conversion is only lossless for color spaces that have a tag,
-// this is why we have this additional trait bound. See also
-// https://github.com/linebender/color/pull/155 for more discussion.
+/// Note that the conversion is only lossless for color spaces that have a tag,
+/// this is why we have this additional trait bound. See also
+/// https://github.com/linebender/color/pull/155 for more discussion.
 impl<CS: ColorSpace> From<AlphaColor<CS>> for DynamicColor
 where
     ColorSpaceTag: From<CS>,
